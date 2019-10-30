98f96a8d32cd8782b3db9f09d8adea0e0e24d23b
Maraz Mia <43583025+MarazMia@users.noreply.github.com>
2019-10-30 13:11:46 +0600

    Update seidel.m

--- labfinalnumericalAnalysis/seidel.m
+++ labfinalnumericalAnalysis/seidel.m
@@ -1,37 +1,36 @@
+#Dear sir,I used while loop instead of
+# for loop in exam paper......
+
 a=[10,-5,-2;4,-10,3;1,6,10];
 b=[3;-3;3];
 n=length(b);
 x=zeros(n,1);
 xnew=zeros(n,1);
 x(:)=0;
-it=0;
-while it<100
+itr=1;
+for itr=1:100
   convergence=true;
-  i=0;
-  while i<n
+  for i=1:n
     sum1=0;
     sum2=0;
-    j=0;
-    while j<i-1
-      sum1=sum1+(a(i,j)*x(j));
-      j=j+1;
-    endwhile
-    j=i+1;
-    while j<n
-      sum2=sum2+(a(i,j)*x(j));
-      j=j+1;
-    endwhile
+    for j=1:i-1
+        sum1=sum1+a(i,j)*x(j);
+    end
+    for m=i+1:n
+        sum2=sum2+a(i,m)*x(m);
+    end
     sum=sum1+sum2;
-    xnew(i)=(-1/a(i,i))*(sum-b(i));
-    if abs((xnew(i)-x(i)))>0.000001
+    xnew(i)=-1/a(i,i)*(sum-b(i));
+    if abs(xnew(i)-x(i))>tol
       convergence=false;
-    endif
-    i=i+1;
-  endwhile
-  if convergence
-    break
-  endif
-  it=it+1;
-endwhile
-disp('solution is')
-xnew
+    end
+   end
+   if convergence
+     break;
+   end
+   x=xnew;
+ end 
+ disp('iterations');
+ itr
+ disp('solutions');
+ xnew
