3bd70902e085dd969744b17b09c1af01cdd83f16
Maraz Mia <43583025+MarazMia@users.noreply.github.com>
2019-10-30 13:17:20 +0600

    Update est.m

--- labfinalnumericalAnalysis/est.m
+++ labfinalnumericalAnalysis/est.m
@@ -1,23 +1,29 @@
+b=[3;-3;3];
+n=length(b);
 x=zeros(n,1);
 xnew=zeros(n,1);
 x(:)=0;
-for it=1:100
+itr=1;
+for itr=1:100
   convergence=true;
   for i=1:n
-    sum1=0;
+    sum=0;
     for j=1:n
-      if i~=j
-      sum1=sum1+a(i,j)*x(j);
-      endif
-    endfor
-    xnew(i)=(-1/(a(i,i)))*(sum1-b(i));
-    if abs((xnew(i)-x(i)))>0.000001
+      if j~=i
+        sum=sum+a(i,j)*x(j);
+      end
+    end
+    xnew(i)=-1/a(i,i)*(sum-b(i));
+    if abs(xnew(i)-x(i))>tol
       convergence=false;
-    endif
-  endfor
-  if convergence
-    break
-  endif
-endfor
-disp('solution is')
-xnew
+    end
+   end
+   if convergence
+     break;
+   end
+   x=xnew;
+ end 
+ disp('iterations');
+ itr
+ disp('solutions');
+ xnew
